3d75989d87924c87261b8c4418eba16e4f88215d
JoaquinJRA <joaquin0420004@gmail.com>
2026-06-17 21:57:40 -0500

    api UPDATE

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://clinica-frontend-rosy-six.vercel.app/login",
+  baseURL: "https://clinicabackend-9qp3.onrender.com/api",
   withCredentials: true,
 });
 
