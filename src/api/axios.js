b4a07f285bfffdc5fc0f12adbc608a8aaa3aef67
JoaquinJRA <joaquin0420004@gmail.com>
2026-07-07 12:17:38 -0500

    Correcciones - 07

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://clinicabackend-9qp3.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
   withCredentials: true,
 });
 
