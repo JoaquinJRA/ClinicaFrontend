eccce8e4f01de44b906fc86b1b66b074538939e9
JoaquinJRA <joaquin0420004@gmail.com>
2026-07-11 18:45:03 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  (import.meta.env.PROD
-    ? "https://clinicabackend-9qp3.onrender.com/api"
-    : "http://localhost:3000/api");
+const API_URL = import.meta.env.PROD
+  ? "https://clinicabackend-9qp3.onrender.com/api"
+  : "http://localhost:3000/api";
 
 const api = axios.create({
   baseURL: API_URL,
